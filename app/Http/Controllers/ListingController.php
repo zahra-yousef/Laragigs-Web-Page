b0c4c07bb1cc788d262e0908ac1d2ac1bab7c5a9
zahra-yousef <zahraaljafar.ipe@gmail.com>
2022-08-30 15:06:54 +0300

    1st commit

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    //show all listing
+    public function index(){
+        return view('listings.index',[
+            'listings' => Listing::all()
+        ]);
+    }
+
+
+    //show single listing
+    public function show(Listing $listing){
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+}
